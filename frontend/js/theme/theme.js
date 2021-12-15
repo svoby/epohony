5b57be8f27cd33c32b854529d32fdece53316ee3
Luboš Svoboda <lubos.svoboda@gmail.com>
2021-12-15 23:13:12 +0100

    plus minus

--- frontend/js/theme/theme.js
+++ frontend/js/theme/theme.js
@@ -13,4 +13,23 @@ spUtils.$document.ready(() => {
         headroom.init();
     }
 
+    // Plus - minu
+    $(document).on('click', '[data-js="plusminus"] button', function (e) {
+        var el = $(this).parent().find('input');
+        var num = parseInt(el.val());
+        num = $(this).hasClass('js-plus') ? num + 1 : num - 1;
+        if (num <= 1) num = 1;
+        el.val(num);
+        el.trigger('change');
+        e.preventDefault();
+    });
+
+    $(document).on('input', '[data-js="plusminus"] input', function () {
+        var num = $(this).val().replace(/[^0-9]/g, '');
+        if (num.length > 2) num = num.substr(0, 2);
+        if (num == 0) num = 1;
+        $(this).val(num);
+        $(this).trigger('change');
+    });
+
 });
